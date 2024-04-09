c7153bddf2fb3b1ef00359100e2733b734996bf2
Wiqiro <william.imbert@utbm.fr>
2024-04-09 11:05:49 +0200

    feat: added User model

---
+++ app/src/main/java/com/collogestion/data/User.kt
@@ -0,0 +1,11 @@
+package com.collogestion.data
+
+class User(
+    var id: Int,
+    var firstname: String,
+    var lastname: String,
+    var profilePicture: Int,
+    var mail: String,
+    var phone: String,
+    var Password: String
+)
