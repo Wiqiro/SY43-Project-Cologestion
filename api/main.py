8e25411e2e87d524f246d309aabb02927991a861
Wiqiro <william.imbert@utbm.fr>
2024-04-11 11:00:43 +0200

    feat: added empty api routers

--- api/main.py
+++ api/main.py
@@ -4,10 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
 
+from routers import (auth, dues, events, groceries, houseshares, tasks, users)
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ColoGestionAPI", redoc_url=None, docs_url="/docs")
-# app.include_router(...)
+app.include_router(auth.router)
+app.include_router(dues.router)
+app.include_router(events.router)
+app.include_router(groceries.router)
+app.include_router(houseshares.router)
+app.include_router(tasks.router)
+app.include_router(users.router)
 
 
 origins = [
---
+++ api/routers/houseshares.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/houseshares", tags=["House Shares"])
